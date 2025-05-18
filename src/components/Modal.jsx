caab68f1837115985272fb1ef52208c5798a64df
Caiko <marinenikolov@gmail.com>
2025-05-18 09:57:17 +0200

    Fixed version and added react 19 ref

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+export default function Modal({ children, ref }) {
   const dialog = useRef();
 
   useImperativeHandle(ref, () => {
@@ -19,8 +19,6 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
-});
-
-export default Modal;
+}
